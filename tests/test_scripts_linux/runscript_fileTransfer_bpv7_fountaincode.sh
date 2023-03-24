8e9a9ca669afd0e731b8f6ad41866fca6f07de82
neop216 <neop216@gmail.com>
2023-03-24 18:30:00 -0400

    first fountain code attempt

---
+++ tests/test_scripts_linux/runscript_fileTransfer_bpv7_fountaincode.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# path variables
+config_files=$HDTN_SOURCE_ROOT/config_files
+hdtn_config=$config_files/hdtn/hdtn_ingress1tcpclv4_port4556_egress1tcpclv4_port4558flowid2.json
+sink_config=$config_files/inducts/bpsink_one_tcpclv4_port4558.json
+gen_config=$config_files/outducts/bpgen_one_tcpclv4_port4556.json
+
+cd $HDTN_SOURCE_ROOT
+
+# BP Receive File
+./build/common/bpcodec/apps/bpreceivefile --save-directory=received --my-uri-eid=ipn:2.1 --inducts-config-file=$sink_config &
+bpreceive_PID=$!
+sleep 3
+
+# HDTN one process
+./build/module/hdtn_one_process/hdtn-one-process --contact-plan-file=contactPlanCutThroughMode.json --hdtn-config-file=$hdtn_config &
+one_process_PID=$!
+sleep 6
+
+# Fountain Code Encoder
+./build/common/fountain_code/encode.py test.txt
+encoder_PID=$!
+sleep 8
+
+# BP Send File 
+./build/common/bpcodec/apps/bpsendfile  --use-bp-version-7  --max-bundle-size-bytes=1000 --file-or-folder-path=encodefile.gz --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:2.1 --outducts-config-file=$gen_config &
+bpsend_PID=$!
+sleep 8
+
+# Fountain Code Decoder
+./build/common/fountain_code/decode.py ./received/encodefile.gz
+decoder_PID=$!
+sleep 8
+
+# cleanup
+sleep 55
+echo "\nkilling decoder..." && kill -2 $decoder_PID
+sleep 2
+echo "\nkilling bp send file..." && kill -2 $bpsend_PID
+sleep 2
+echo "\nkilling encoder..." && kill -2 $encoder_PID
+sleep 2
+echo "\nkilling HDTN one process ..." && kill -2 $one_process_PID
+sleep 2
+echo "\nkilling bp receive file..." && kill -2 $bpreceive_PID
+
+
